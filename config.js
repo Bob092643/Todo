b473587f7f760acc9c9d0811fce6316a94fc86a5
Bob092643 <r.boot@qquest.nl>
2026-09-18 18:07:46 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAQTCMYDq__QSdVHE7l2gWIEfiPfRekw7g",
+    authDomain: "to-do-s-83b65.firebaseapp.com",
+    projectId: "to-do-s-83b65",
+    storageBucket: "to-do-s-83b65.firebasestorage.app",
+    messagingSenderId: "171312632803",
+    appId: "1:171312632803:web:2ef5fe48d4b9d23fb646d6",
+  },
+};
